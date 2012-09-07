77be79c644d52cfb2053900a5051d902abaf4447
Y NIshida <glory791days@gmail.com>
2012-09-07 13:28:53 -0400

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+YnishidaInd::Application.config.secret_token = '6bb42ac8a13991617d7d7036effcbb9f41e7a6b4bd93f6f1c7abbaf36f90209ddddb21b12181c85fc64893a150c46b7730de5e4f06d54ea7ed31a3362938153d'
