20162bb26281eb67d938df9949afc60b369689aa
glory791days <glory791days@gmail.com>
2012-10-03 16:37:01 -0400

    modified styles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+	# Returns the full title on a per-page basis.
+  # @param [Object] page_title
+	def full_title(page_title)
+    base_title = "Bancha"
+    if page_title.empty?
+      "#{base_title}: Find your favorite cafe at CMU"
+    else
+      "#{base_title} | #{page_title}"
+    end
+  end
 end
